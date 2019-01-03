d6861b294072ce048defa10b83ac965f48b770cc
Marek Faltysek <marek.faltysek@praguematica.co.uk>
2019-01-03 18:30:12 +0100

    build process

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
-go test ./... -race -coverprofile=coverage.txt -covermode=atomic
+set -e
+echo "" > coverage.txt
+
+for d in $(go list ./... | grep -v vendor); do
+    go test -race -coverprofile=profile.out -covermode=atomic $d
+    if [ -f profile.out ]; then
+        cat profile.out >> coverage.txt
+        rm profile.out
+    fi
+done
