ac91a06f4372068860d0ac2002409336208a53ff
Marek Faltysek <marek.faltysek@praguematica.co.uk>
2019-01-09 15:09:46 +0000

    Adding 3 sec timeout for 'test url' functionality

--- internal/pkg/local/http.go
+++ internal/pkg/local/http.go
@@ -4,14 +4,17 @@ import (
 	"fmt"
 	"github.com/pgmtc/orchard-cli/internal/pkg/common"
 	"net/http"
+	"time"
 )
 
 func isResponding(cmp common.Component) string {
+	timeout := time.Duration(3 * time.Second)
 	if cmp.TestUrl == "" {
 		return ""
 	}
 
 	client := &http.Client{
+		Timeout: timeout,
 		CheckRedirect: func(req *http.Request, via []*http.Request) error {
 			return http.ErrUseLastResponse
 		},
