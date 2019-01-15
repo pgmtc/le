9c4fc581ed2504b1ea7ce32088dbc540b276de6b
Marek Faltysek <marek.faltysek@praguematica.co.uk>
2019-01-15 16:20:21 +0000

    improving test coverage

--- internal/pkg/docker/runner_docker_test.go
+++ internal/pkg/docker/runner_docker_test.go
@@ -2,6 +2,7 @@ package docker
 
 import (
 	"github.com/pgmtc/orchard-cli/internal/pkg/common"
+	"os"
 	"testing"
 )
 
@@ -9,9 +10,21 @@ var runner = DockerRunner{}
 var ctx = common.Context{
 	Config: common.CreateMockConfig([]common.Component{
 		{
-			Name:     "test-component",
+			Name:          "test-component",
+			DockerId:      "test-component",
+			Image:         "nginx:stable-alpine",
+			ContainerPort: 80,
+			HostPort:      9999,
+			TestUrl:       "http://localhost:9999",
+		},
+		{
+			Name:     "test-internal-component",
 			DockerId: "test-component",
-			Image:    "iron/go",
+			Image:    "674155361995.dkr.ecr.eu-west-1.amazonaws.com/orchard/orchard-local-db:latest",
+		},
+		{
+			Name:     "test-invalid",
+			DockerId: "test-component",
 		},
 	}),
 	Log: common.ConsoleLogger{},
@@ -20,6 +33,8 @@ var ctx = common.Context{
 func TestDockerRunner_Status(t *testing.T) {
 	runner.Status(ctx)
 	runner.Status(ctx, "-v")
+	runner.Status(ctx, "-f", "2")
+	runner.Status(ctx, "-v", "-f", "2")
 }
 
 func TestDockerRunner_Pull(t *testing.T) {
@@ -32,6 +47,8 @@ func TestDockerRunner_Pull(t *testing.T) {
 
 func TestDockerRunner_Workflow(t *testing.T) {
 	cmp := ctx.Config.CurrentProfile().Components[0]
+	internalCmp := ctx.Config.CurrentProfile().Components[1]
+	invalidCmp := ctx.Config.CurrentProfile().Components[2]
 
 	runner.Remove(ctx, cmp) // Don't handle errors - prepare for the test
 	removeImage(cmp)        // Don't handle errors - prepare for the test
@@ -41,8 +58,26 @@ func TestDockerRunner_Workflow(t *testing.T) {
 	}
 	defer removeImage(cmp)
 
+	runner.Remove(ctx, internalCmp) // Don't handle errors - prepare for the test
+	removeImage(internalCmp)        // Don't handle errors - prepare for the test
+	if err := runner.Pull(ctx, internalCmp); err != nil && !(os.Getenv("SKIP_AWS_TESTING") == "") {
+		t.Errorf("Unexpected error: %s", err.Error())
+	}
+	defer removeImage(internalCmp)
+
+	runner.Remove(ctx, invalidCmp) // Don't handle errors - prepare for the test
+	removeImage(invalidCmp)        // Don't handle errors - prepare for the test
+	if err := runner.Pull(ctx, invalidCmp); err == nil {
+		t.Errorf("Expected error, got nothing")
+	}
+	defer removeImage(invalidCmp)
+
 	if err := runner.Create(ctx, cmp); err != nil {
 		t.Errorf("Unexpected error: %s", err.Error())
+	}
+	// Recreate again - should get error
+	if err := runner.Create(ctx, cmp); err == nil {
+		t.Errorf("Expected error, got nothing")
 	}
 	defer runner.Remove(ctx, cmp)
 
@@ -50,14 +85,14 @@ func TestDockerRunner_Workflow(t *testing.T) {
 		t.Errorf("Unexpected error: %s", err.Error())
 	}
 
-	if err := runner.Stop(ctx, cmp); err != nil {
-		t.Errorf("Unexpected error: %s", err.Error())
-	}
-
 	if err := runner.Logs(ctx, cmp, false); err != nil {
 		t.Errorf("Unexpected error: %s", err.Error())
 	}
 
+	if err := runner.Stop(ctx, cmp); err != nil {
+		t.Errorf("Unexpected error: %s", err.Error())
+	}
+	runner.Create(ctx, cmp) // Don't handle errors
 	if err := runner.Remove(ctx, cmp); err != nil {
 		t.Errorf("Unexpected error: %s", err.Error())
 	}
