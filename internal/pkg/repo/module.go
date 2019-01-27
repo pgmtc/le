07b0135ca810725e5e56554d5e64f16e40c29ee1
Marek Faltysek <marek.faltysek@praguematica.co.uk>
2019-01-27 20:09:14 +0000

    Adding simple repo module (prints repository url)

---
+++ internal/pkg/repo/module.go
@@ -0,0 +1,13 @@
+package repo
+
+import (
+	"github.com/pgmtc/orchard-cli/internal/pkg/common"
+)
+
+type Module struct{}
+
+func (Module) GetActions() map[string]common.Action {
+	return map[string]common.Action{
+		"url": &urlAction,
+	}
+}
